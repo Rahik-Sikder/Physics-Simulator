41d162a4a9ecf0a77324103b90cf15392e9b7b67
Rahik-Sikder <96138506+Rahik-Sikder@users.noreply.github.com>
2022-06-07 16:38:41 -0500

    Particles bouncing in a box

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-from tabnanny import check
-from turtle import update
 import pygame, sys
 
 
 WIDTH, HEIGHT = 900, 500
-FLOOR = HEIGHT - 10
+BORDER = 10
+BOX = pygame.Rect(BORDER, BORDER, WIDTH - 2 * BORDER, HEIGHT - 2 * BORDER)
 # telling pygame to make a new window
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2D Physics Simulator")
 
 WHITE = (255,255,255)
 BLACK = (0,0,0)
-GRAVITY = 9.8
+RED = (255, 0, 0)
 
-blocks = pygame.sprite.Group()
+FPS = 60
+DELTA_TIME = 1 / FPS
+GRAVITY = 200
+ENERGY_CONSERVED = .75 # percent
+
+particles = []
 
 def main():
     
     clock = pygame.time.Clock()
     run = True
     while run:
-        clock.tick(30)
+        clock.tick(100)
         
-        WIN.fill(BLACK)
-        pygame.draw.rect(WIN, WHITE, pygame.Rect(0,FLOOR, WIDTH, 10))
         
-        blocks.update()
+        WIN.fill(RED)
+        pygame.draw.rect(WIN, BLACK, BOX)
         
+        for particle in particles:
+            update(particle)
         
         for event in pygame.event.get():
             # Checking if quit
@@ -38,73 +43,75 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
-                blocks.add(Square(pos[0],pos[1]))
+                p = Particle(pos,10)
+                particles.append(p)
 
         pygame.display.update()
     
+def update(particle):
     
+    particle.force = [0, GRAVITY]
     
+    check_collision(particle)  
+    
+    particle.velocity[0] = particle.velocity[0] + particle.force[0] * DELTA_TIME / particle.mass
+    particle.velocity[1] = particle.velocity[1] + particle.force[1] * DELTA_TIME / particle.mass
+    
+    particle.position[0] = particle.position[0] + particle.velocity[0] * DELTA_TIME 
+    particle.position[1] = particle.position[1] + particle.velocity[1] * DELTA_TIME
+    
+    #print(particle.position)
+    particle.update_cords()
+    particle.draw()
 
-class Square(pygame.sprite.Sprite):
+def check_collision(particle):
+    collision = False
+    # top
+    if(particle.top <= BOX.top):
+        particle.velocity[1] = abs(particle.velocity[1])
+        particle.force[1] += -particle.force[1]
+        collision = True
+        
+    # bottom
+    if(particle.bottom >= BOX.bottom):
+        particle.velocity[1] = -1 * abs(particle.velocity[1])
+        particle.force[1] += -particle.force[1]
+        collision = True
+        
+    # left
+    if(particle.left <= BOX.left):
+        particle.velocity[0] = abs(particle.velocity[0])
+        particle.force[0] += -particle.force[0]
+        collision = True
     
-    velocity = 0
-    force = 0
+    # right
+    if(particle.right >= BOX.right):
+        particle.velocity[0] = -1 * abs(particle.velocity[0])
+        particle.force[0] += -particle.force[0]
+        collision = True
+        
+
+class Particle:
     
-    def __init__(self, x, y, height=50, width=50, mass=10):
-        pygame.sprite.Sprite.__init__(self)
-        self.x = x
-        self.y = y
-        self.height = height
-        self.width = width
+    def __init__(self, position, radius, mass=1):
+        self.position = list(position)
+        self.velocity = [500,0]
+        self.force = [5000,GRAVITY * mass]
         self.mass = mass
-        self.rect = pygame.Rect(self.x, self.y, self.height, self.width)
-        self.draw()
+        self.radius = radius
+        self.top = position[1] - radius
+        self.bottom = position[1] + radius
+        self.left = position[0] - radius
+        self.right = position[0] + radius
     
-    def set_pos(self, pos):
-        self.x = pos[0]
-        self.y = pos[1]
-    
+    def update_cords(self):
+        self.top = self.position[1] - self.radius
+        self.bottom = self.position[1] + self.radius
+        self.left = self.position[0] - self.radius
+        self.right = self.position[0] + self.radius
+        
     def draw(self):
-        pygame.draw.rect(WIN, WHITE, self.rect)
-        
-    def update(self):
-        self.force = 0
-        self.force = GRAVITY * self.mass
-        # Euler integration
-        self.velocity += self.force * 1 / self.mass
-        self.y += self.velocity * 1 
-        
-        
-        self.checkFloorCollision()
-        self.rect = pygame.Rect(self.x, self.y, self.height, self.width)
-        self.draw()
-    
-    def checkFloorCollision(self): 
-        if(self.velocity != 0 and self.y + self.height >= FLOOR):
-            self.y = FLOOR - self.height
-            self.velocity = -1 * self.velocity
-            #print(self.velocity)
-            self.update()
-
-    def collided_with_another_rect(self, rect):
-        """ Assumes rectangles are same size or that this rectangle is smaller than the other rectangle"""
-        if self.x > (rect.x + rect.width):
-            # Is to the right of the other rectangle
-            return False
-        elif (self.x + self.width) < rect.x:
-            # is to the left of the other rectangle
-            return False
-        elif (self.y + self.height) < rect.y:
-            # is above the other rectangle
-            return False
-        elif self.y > (rect.y + rect.height):
-            # is below the other rectangle
-            return False
-        else:
-            return True
-
-    
-    
+        pygame.draw.circle(WIN, WHITE, self.position, self.radius)
 
 
     
